58f24e416f5660fbf5bf21b9d696bc6fa2438613
Vect0r-1 <victoradewale.amv@gmail.com>
2023-02-22 15:46:07 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar directs the charactr c to stout
+ * c is the character to print
+ *
+ * Return: 1 (On success)
+ * On error: -1, and errno is set.
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
