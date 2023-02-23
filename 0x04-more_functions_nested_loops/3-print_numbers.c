6d56df101861f06f828aec38987a927c6ce28b66
Vect0r-1 <victoradewale.amv@gmail.com>
2023-02-23 13:53:08 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * followed by a new line.
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int c = 0;
+
+	while (c >= 0 && c <= 9)
+	{
+		_putchar('c');
+		c++
+	}
+	_putchar('\n');
+	return (0);
+}
