a0fec8a158b025ac73c963d033aea21b5d72b4a4
Vect0r-1 <victoradewale.amv@gmail.com>
2023-02-22 16:00:52 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all alphabets in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	_putchar('\n');
+}
