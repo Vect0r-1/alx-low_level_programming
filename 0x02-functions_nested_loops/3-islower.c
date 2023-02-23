f2cb85715ecd6956af1360022d91ee85d46ed7bf
Vect0r-1 <victoradewale.amv@gmail.com>
2023-02-22 16:43:00 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks if the character is lowercase
+ * @c: is the character to be checked
+ *
+ * Retuen: 1 if character is lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
