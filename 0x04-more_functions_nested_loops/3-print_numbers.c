8803e700efd0a366536456d82fb18229b27eba32
Vect0r-1 <victoradewale.amv@gmail.com>
2023-02-23 14:25:01 -0800

    Task 4 & 5

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,14 +7,11 @@
  */
 void print_numbers(void)
 {
-	int c;
+	char c;
 
-	c = 0;
-	while (c >= '0' && c <= '9')
+	for (c >= '0'; c <= '9'; c++)
 	{
-		_putchar('c');
-		c++
+		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }
