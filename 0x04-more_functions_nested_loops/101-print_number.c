150ca88472e8b491f217e87186f2b20e155e6550
Vect0r-1 <victoradewale.amv@gmail.com>
2023-02-23 18:43:18 -0800

    Adjustments

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,15 +12,14 @@ void print_number(int n)
 	{
 		n1 = -n;
 		_putchar('-');
-	} else
+	}
+	else
 	{
 		n1 = n;
 	}
-
 	if (n1 / 10)
 	{
 		print_number(n1 / 10);
 	}
-
 	_putchar((n1 % 10) + '0');
 }
